38967af55d9cc5829357593d096cccd29851bcbe
Matt McGinnis <mcginnisma90@gmail.com>
2018-10-10 10:13:43 -0400

    Initialized e-commerce site.

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'categorical' => 'storefront#items_by_category'
+  get 'branding' => 'storefront#items_by_brand'
+  resources :products
+
+  root 'storefront#all_items'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
